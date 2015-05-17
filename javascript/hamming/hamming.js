006fabb0b130240eae1c3d179c1aa0b9d7cbebd7
jozr <jozright@gmail.com>
2015-05-16 21:11:50 -0700

    Update solution

--- javascript/hamming/hamming.js
+++ javascript/hamming/hamming.js
@@ -1,12 +1,9 @@
 exports.compute = function(strand1, strand2) {
-  if (strand1.length != strand2.length) throw new Error('DNA strands must be of equal length.')
+  if (strand1.length !== strand2.length) throw new Error('DNA strands must be of equal length.');
   
-  var arr1 = strand1.split('')
-  var arr2 = strand2.split('')
-  var count = 0
-
-  for (var i = 0, len = arr1.length; i < len; i++) {
-    if (arr1[i] != arr2[i]) count += 1
+  var count = 0;
+  for (var i = 0, len = strand1.length; i < len; i++) {
+    if (strand1[i] !== strand2[i]) count ++;
   };
-  return count
+  return count;
 };
