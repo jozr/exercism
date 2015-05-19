be7129bec9428782f8b4abdd2f335ae340030de9
jozr <jozright@gmail.com>
2015-05-18 20:20:52 -0700

    Finish solution

---
+++ javascript/hello-world/hello-world.js
@@ -0,0 +1,11 @@
+var HelloWorld = function() {};
+
+HelloWorld.prototype.hello = function(input) {
+  if (input) {
+    return 'Hello, ' + input + '!'
+  } else {
+    return 'Hello, world!'
+  }
+};
+
+module.exports = HelloWorld;
