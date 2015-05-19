bda88ca158978d7d9951420f6cd9b53c75e7bb53
jozr <jozright@gmail.com>
2015-05-18 20:06:56 -0700

    First solution of anagram

--- javascript/anagram/anagram.js
+++ javascript/anagram/anagram.js
@@ -2,13 +2,18 @@ var anagram = function(word) { return new Anagram(word); }
 
 function Anagram(word) { this.word = word.toLowerCase(); };
 
-Anagram.prototype.matches = function(list) {
-  var orderedWord = this.word.toLowerCase().split('').sort();
+Anagram.prototype.matches = function(context) {
   var results = [];
-  for (var i = 0; i < list.length; i++) {
-    var orderedCurrent = list[i].toLowerCase().split('').sort();
-    if (orderedWord.join() === orderedCurrent.join()) {
-      results.push(list[i]);
+  var args = Array.prototype.slice.call(arguments, 0);
+
+  if (args[0] instanceof Array) args = args[0];
+
+  for (var i = 0; i < args.length; i++) {
+    var orderedCurrent = args[i].toLowerCase().split('').sort();
+
+    if (this.word.split('').sort().join() === orderedCurrent.join() 
+      && this.word != args[i].toLowerCase()) {
+        results.push(args[i]);
     }
   }
   return results;
