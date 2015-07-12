fd9cc9800d775ae69f7d194fcb2b512edd08d627
“jozr” <jozright@gmail.com>
2015-07-11 19:00:09 -0700

    Pass second test!

---
+++ javascript/food-chain/food-chain.js
@@ -0,0 +1,6 @@
+exports.verse = function(input) {
+  if (input === 2) result = "I know an old lady who swallowed a spider.\nIt wriggled and jiggled and tickled inside her.\nShe swallowed the spider to catch the fly.\n"
+  if (input === 1) result = "I know an old lady who swallowed a fly.\n"
+  result += "I don't know why she swallowed the fly. Perhaps she'll die.\n"
+  return result
+};
