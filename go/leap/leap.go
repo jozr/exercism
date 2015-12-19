1d2629744cd8f4124cc5edc4372c3d0bcccbcb1a
“jozr” <jozright@gmail.com>
2015-12-18 23:45:45 -0800

    go/leap: PASS tests

--- go/leap/leap.go
+++ go/leap/leap.go
@@ -2,11 +2,10 @@ package leap
 
 const TestVersion = 1
 
-// It's good style to write a comment here documenting IsLeapYear.
-func IsLeapYear(int) bool {
-  if int % 400 == 0 || (int % 4 == 0 && !(int % 100 == 0)) {
+func IsLeapYear(year int) bool {
+  if year % 400 == 0 || (year % 4 == 0 && !(year % 100 == 0)) {
     return true
-  } else
+  } else {
     return false
   }
 }
