294533cf8ac840e469cf6dbbcf7916add4737445
“jozr” <jozright@gmail.com>
2016-09-04 21:05:18 -0700

    Passing!!!!

--- javascript/food-chain/food-chain.js
+++ javascript/food-chain/food-chain.js
@@ -1,23 +1,59 @@
-exports.verse = function(verse) {
-  var result = "I know an old lady who swallowed a ";
-  if (verse === 8) {
-    result += "horse.\nShe's dead, of course!\n";
-    return result;
+exports.verse = function(start, end) {
+  var result = "";
+  if (end == undefined) end = 1;
+
+
+  for (var i = 0; i < end; i++) {
+    result += "I know an old lady who swallowed a ";
+    if (start === 8) {
+      result += "horse.\nShe's dead, of course!\n";
+      return result;
+    };
+    if (start === 7) result += "cow.\nI don't know how she swallowed a cow!\n";
+    if (start > 6)   result += "She swallowed the cow to catch the goat.\n";
+    if (start === 6) result += "goat.\nJust opened her throat and swallowed a goat!\n";
+    if (start > 5)   result += "She swallowed the goat to catch the dog.\n";
+    if (start === 5) result += "dog.\nWhat a hog, to swallow a dog!\n";
+    if (start > 4)   result += "She swallowed the dog to catch the cat.\n";
+    if (start === 4) result += "cat.\nImagine that, to swallow a cat!\n";
+    if (start > 3)   result += "She swallowed the cat to catch the bird.\n";
+    if (start === 3) result += "bird.\nHow absurd to swallow a bird!\n";
+    if (start > 2)   result += "She swallowed the bird to catch the spider that ";
+    if (start === 2) result += "spider.\nIt ";
+    if (start > 1)   result += "wriggled and jiggled and tickled inside her.\nShe swallowed the spider to catch the fly.\n";
+    if (start === 1) result += "fly.\n";
+    result += "I don't know why she swallowed the fly. Perhaps she'll die.\n";
   };
-  if (verse === 7) result += "cow.\nI don't know how she swallowed a cow!\n";
-  if (verse > 6)   result += "She swallowed the cow to catch the goat.\n";
-  if (verse === 6) result += "goat.\nJust opened her throat and swallowed a goat!\n";
-  if (verse > 5)   result += "She swallowed the goat to catch the dog.\n";
-  if (verse === 5) result += "dog.\nWhat a hog, to swallow a dog!\n";
-  if (verse > 4)   result += "She swallowed the dog to catch the cat.\n";
-  if (verse === 4) result += "cat.\nImagine that, to swallow a cat!\n";
-  if (verse > 3)   result += "She swallowed the cat to catch the bird.\n";
-  if (verse === 3) result += "bird.\nHow absurd to swallow a bird!\n";
-  if (verse > 2)   result += "She swallowed the bird to catch the spider that ";
-  if (verse === 2) result += "spider.\nIt ";
-  if (verse > 1)   result += "wriggled and jiggled and tickled inside her.\nShe swallowed the spider to catch the fly.\n";
-  if (verse === 1) result += "fly.\n";
-  result += "I don't know why she swallowed the fly. Perhaps she'll die.\n";
+
+  return result;
+};
+
+exports.verses = function(start, end) {
+  var result = "";
+  if (end == undefined) end = 1;
+
+  for (var i = 0; i < end; i++) {
+    result += "I know an old lady who swallowed a ";
+    if (start === 8) {
+      result += "horse.\nShe's dead, of course!\n\n";
+      return result;
+    };
+    if (start === 7) result += "cow.\nI don't know how she swallowed a cow!\n";
+    if (start > 6)   result += "She swallowed the cow to catch the goat.\n";
+    if (start === 6) result += "goat.\nJust opened her throat and swallowed a goat!\n";
+    if (start > 5)   result += "She swallowed the goat to catch the dog.\n";
+    if (start === 5) result += "dog.\nWhat a hog, to swallow a dog!\n";
+    if (start > 4)   result += "She swallowed the dog to catch the cat.\n";
+    if (start === 4) result += "cat.\nImagine that, to swallow a cat!\n";
+    if (start > 3)   result += "She swallowed the cat to catch the bird.\n";
+    if (start === 3) result += "bird.\nHow absurd to swallow a bird!\n";
+    if (start > 2)   result += "She swallowed the bird to catch the spider that ";
+    if (start === 2) result += "spider.\nIt ";
+    if (start > 1)   result += "wriggled and jiggled and tickled inside her.\nShe swallowed the spider to catch the fly.\n";
+    if (start === 1) result += "fly.\n";
+    result += "I don't know why she swallowed the fly. Perhaps she'll die.\n\n";
+    start++;
+  };
 
   return result;
 };
