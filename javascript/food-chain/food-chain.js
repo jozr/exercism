ca68446e0d618010371b5c6ccc93c560bd6cee1b
“jozr” <jozright@gmail.com>
2016-09-07 22:42:43 -0700

    Almost there

--- javascript/food-chain/food-chain.js
+++ javascript/food-chain/food-chain.js
@@ -1,66 +1,63 @@
-var self = module.exports = {
-  verse: function(verse, multi) {
-    var list = {
-      "8": {
-        "unique": "horse.\nShe's dead, of course!\n",
-        "default": "I know an old lady who swallowed a "
-      },
-      "7": {
-        "unique": "cow.\nI don't know how she swallowed a cow!\n",
-        "default": "She swallowed the cow to catch the goat.\n"
-      },
-      "6": {
-        "unique": "goat.\nJust opened her throat and swallowed a goat!\n",
-        "default": "She swallowed the goat to catch the dog.\n"
-      },
-      "5": {
-        "unique": "dog.\nWhat a hog, to swallow a dog!\n",
-        "default": "She swallowed the dog to catch the cat.\n"
-      },
-      "4": {
-        "unique": "cat.\nImagine that, to swallow a cat!\n",
-        "default": "She swallowed the cat to catch the bird.\n"
-      },
-      "3": {
-        "unique": "bird.\nHow absurd to swallow a bird!\n",
-        "default": "She swallowed the bird to catch the spider that "
-      },
-      "2": {
-        "unique": "spider.\nIt ",
-        "default": "wriggled and jiggled and tickled inside her.\nShe swallowed the spider to catch the fly.\n"
-      },
-      "1": {
-        "unique": "fly.\n",
-        "default": "I don't know why she swallowed the fly. Perhaps she'll die.\n"
-      }
-    };
+exports.verse = function(verse) {
+  return add_verses_to_result(verse, "");
+};
 
-    var result = "";
-    if (multi == undefined) multi = 1;
+exports.verses = function(verse, multi) {
+  var result = "";
 
-    for (var i = multi; i > 0; i--) {
-      result += list[8]["default"];
+  for (var i = multi; i > 0; i--) {
+    result = add_verses_to_result(verse, result) + "\n";
+    verse++;
+  };
 
-      if (verse === 8) {
-        result += list[8]["unique"];
-        if (multi !== 1) result += "\n";
-        return result;
-      };
+  return result;
+};
 
-      for (var j = verse; j > 0; j--) {
-        if (verse === j) result += list[j]["unique"];
-        if (verse >= j)  result += list[j]["default"];
-      };
+var add_verses_to_result = function(verse, result) {
+  result += list()[8]["default"];
 
-      if (multi !== 1) result += "\n";
+  for (var i = verse; i > 0; i--) {
+    if (verse === i) result += list()[i]["unique"];
+    if (verse === 8) break;
+    if (verse >= i) result += list()[i]["default"];
+  };
 
-      verse++;
-    };
+  return result;
+};
 
-    return result;
-  },
-
-  verses: function(verse, multi) {
-    return self.verse(verse, multi);
-  }
+var list = function() {
+ return {
+    "8": {
+      "unique": "horse.\nShe's dead, of course!\n",
+      "default": "I know an old lady who swallowed a "
+    },
+    "7": {
+      "unique": "cow.\nI don't know how she swallowed a cow!\n",
+      "default": "She swallowed the cow to catch the goat.\n"
+    },
+    "6": {
+      "unique": "goat.\nJust opened her throat and swallowed a goat!\n",
+      "default": "She swallowed the goat to catch the dog.\n"
+    },
+    "5": {
+      "unique": "dog.\nWhat a hog, to swallow a dog!\n",
+      "default": "She swallowed the dog to catch the cat.\n"
+    },
+    "4": {
+      "unique": "cat.\nImagine that, to swallow a cat!\n",
+      "default": "She swallowed the cat to catch the bird.\n"
+    },
+    "3": {
+      "unique": "bird.\nHow absurd to swallow a bird!\n",
+      "default": "She swallowed the bird to catch the spider that "
+    },
+    "2": {
+      "unique": "spider.\nIt ",
+      "default": "wriggled and jiggled and tickled inside her.\nShe swallowed the spider to catch the fly.\n"
+    },
+    "1": {
+      "unique": "fly.\n",
+      "default": "I don't know why she swallowed the fly. Perhaps she'll die.\n"
+    }
+  };
 };
